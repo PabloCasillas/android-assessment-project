f31e06b2f39004981f5ebeb20b11fe10587f7657
Pablo Casillas <pablo.casillas@wembleystudios.com>
2019-11-05 12:41:34 +0100

    1. Challenge: The Wrong State - we took care of fetching the data remotely from the api. So the app is supposed to show a list of posters, but instead it still displays the progress bar. Can you find where the problem is and fix it? And to make sure we won't make the same mistake twice write a simple unit test.

--- features/list/src/test/java/com/vp/list/viewmodel/ListViewModelTest.java
+++ features/list/src/test/java/com/vp/list/viewmodel/ListViewModelTest.java
@@ -3,13 +3,19 @@ package com.vp.list.viewmodel;
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 import androidx.lifecycle.Observer;
 
+import com.vp.list.model.ListItem;
 import com.vp.list.model.SearchResponse;
 import com.vp.list.service.SearchService;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.internal.verification.AtLeast;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.mock.Calls;
 
@@ -52,6 +58,35 @@ public class ListViewModelTest {
 
         //then
         verify(mockObserver).onChanged(SearchResult.inProgress());
+    }
+
+    @Test
+    public void shouldReturnSuccessState() {
+
+        //given
+        SearchService searchService = mock(SearchService.class);
+        SearchResponse searchResponse = mock(SearchResponse.class);
+        List<ListItem> listItems = new ArrayList<>();
+        listItems.add(mock(ListItem.class));
+        listItems.add(mock(ListItem.class));
+
+        when(searchService.search(anyString(), anyInt())).thenReturn(Calls.response(searchResponse));
+        when(searchResponse.getSearch()).thenReturn(listItems);
+        when(searchResponse.getTotalResults()).thenReturn(listItems.size());
+
+        ListViewModel listViewModel = new ListViewModel(searchService);
+        Observer<SearchResult> mockObserver = (Observer<SearchResult>) mock(Observer.class);
+        ArgumentCaptor<SearchResult> captor = ArgumentCaptor.forClass(SearchResult.class);
+        listViewModel.observeMovies().observeForever(mockObserver);
+
+        //when
+        listViewModel.searchMoviesByTitle("title", 1);
+
+        //then
+        verify(mockObserver, new AtLeast(1)).onChanged(captor.capture());
+
+        Assert.assertEquals(SearchResult.inProgress(), captor.getAllValues().get(0));
+        Assert.assertEquals(SearchResult.success(listItems, listItems.size()), captor.getAllValues().get(1));
     }
 
 }
